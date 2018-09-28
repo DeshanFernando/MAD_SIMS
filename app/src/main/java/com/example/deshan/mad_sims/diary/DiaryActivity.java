f0b77247292fc602c0cf398948cf94a8c6636a8a
Ajax <kasun@leoera.com>
2018-09-28 06:19:34 +0530

    First Commit

---
+++ app/src/main/java/com/example/deshan/mad_sims/diary/DiaryActivity.java
@@ -0,0 +1,15 @@
+package com.example.deshan.mad_sims.diary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.deshan.mad_sims.R;
+
+public class DiaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary);
+    }
+}
